45a245779266ef1d9c8b5e9b37afc63a4bc3efad
Jasmin <jasymatos@gmail.com>
2019-10-08 11:26:18 -0400

    reorganized folders and fixed get routes

--- models/Article.js
+++ models/Article.js
@@ -1,27 +1,26 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
 var ArticleSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
-    required: true,
-    unique: true //to prevent repeating
+    required: true
   },
-  // `summary` is required and of type String
-  summary: {
+  // `link` is required and of type String
+  link: {
     type: String,
     required: true
   },
-  
-  date: String,
-  saved: {
-    type: Boolean,
-    default: false
-  }
+  excerpt: {
+    type: String
+  },
+  // `note` is an array that stores a Note id
+  note: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
 });
 
 // This creates our model from the above schema, using mongoose's model method
@@ -29,3 +28,5 @@ var Article = mongoose.model("Article", ArticleSchema);
 
 // Export the Article model
 module.exports = Article;
+
+
--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,105 +1,92 @@
 console.log("index.js is connected")
 
-$(document).ready(function () {
-    //Reference to div that will populate the articles
-    var articleContainer = $(".article-container");
-    //Event listeners to dynamically populated buttons
-    $(document).on("click", ".btn.save", handleArticleSave);
-    $(document).on("click", ".scrape-new", handleArticleScrape);
+//Eventlistener functionalities:
 
-    //Once page is Ready Run this function
-    initPage();
-
-    function initPage() {
-
-        articleContainer.empty();
-        $.get("/api/articles?saved=false")
-            .then(function (data) {
-                if (data && data.length) {
-                    renderArticles(data);
-                } else {
-                    renderEmpty();
-                }
-            });
-    }
-
-    function renderArticles(articles) {
-        //Empty array for panels to be pushed into
-        var articlePanels = [];
-
-        for (var i = 0; articles.legnth; i++) {
-            articlePanels.push(createPanel(articles[i]));
+//Scrape Articles
+$(document).on("click", "#scrape-button", function() {
+    $.ajax({
+      method: "GET",
+      url: "/scrape"
+    })
+    window.location.replace("/scrape");
+  });
+  
+  //Delete an article
+  $(document).on("click", ".delete-article", function() {
+      var thisId = $(this).attr("data-id");
+      $.ajax({
+        method: "DELETE",
+        url: "/saved/" + thisId
+      })
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        location.reload();
+      });
+  });
+  
+  //Save an article
+  $(document).on("click", ".save-article", function() {
+    var thisId = $(this).attr("data-id");
+    $(this).hide();
+    var data = {}
+    data.title =  $("#title-" + thisId).text();
+    data.link = $("#link-" + thisId).text();
+    data.excerpt = $("#excerpt-" + thisId).text();
+    $.ajax({
+      method: "POST",
+      dataType: "json",
+      url: "/api/saved",
+      data: data
+    })
+  });
+  
+  //Go to the notes page for a particular article
+  $(document).on("click", ".note-comment", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    })
+    window.location.replace("/articles/" + thisId);
+  });
+  
+    // Submit a note
+    $(document).on("click", "#submit-note", function() {
+      // Grab the id associated with the article from the submit button
+      var thisId = $(this).attr("data-id");
+        // Run a POST request to save the note
+      $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+          // Value taken from title input
+          title: $("#title-note").val(),
+          // Value taken from note textarea
+          body: $("#note-description").val()
         }
-
-        articleContainer.append(articlePanels);
-    }
-
-    function createPanel(article) {
-
-        var panel =
-            $(["<div class = 'panel panel default'>",
-                "<div class= 'panel-heading'>",
-                "<h3>",
-                article.title,
-                "<a class = 'btn btn-primary save'>",
-                "Save Article",
-                "</a>",
-                "</h3>",
-                "</div>",
-                "<div class = 'panel-body'>",
-                article.summary,
-                "</div>",
-                "</div>"
-            ].join(""));
-
-        panel.date("_id", article._id);
-
-        return panel;
-    }
-
-    function renderEmpty() {
-
-        var emptyAlert =
-            $(["<div class = 'alert alert-warning text-center'>",
-                "<h4>There are no new articles today. Check back tomorrow!</h4>",
-                "</div>",
-                "<div class = 'paanel panel-default'>",
-                "<div class = 'panel-heading text-center'>",
-                "<h3>What would you like to do?</h3>",
-                "</div>",
-                "<div class = 'panel-body text-center'>",
-                "<h4><a class = 'scrape-new'>Scrape New Articles</a></h4>",
-                "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
-                "</div>",
-                "</div>"
-            ].join(""));
-
-        articleContainer.append(emptyAlert);
-    }
-
-    function handleArticleSave() {
-
-        var articleToSave = $(this).parents(".panel").date();
-        articleToSave.saved = true;
-
-        $.ajax({
-            method: "PATCH",
-            url: "/api/articles",
-            date: articleToSave
-        })
-            .then(function (data) {
-                if (data.ok) {
-                    initPage();
-                }
-            })
-    }
-
-    function handleArticleScrape() {
-
-        $.get("/api/fetch")
+      })
         .then(function(data) {
-            initPage();
-            bootbox.alert("<h3 class = 'text-center m-top-80'>" + data.message +"</h3>");
-        });
-    }
-});
+          // Log the response
+          console.log(data);
+          window.location.replace("/articles/" + data._id);
+        });  
+        // Also, remove the values entered in the input and textarea for note entry
+        $("#title-note").val("");
+        $("#note-description").val("");
+    });
+    
+    //delete a note
+    $(document).on("click", ".delete-note", function() {
+      var thisId = $(this).attr("data-id");
+      $.ajax({
+        method: "DELETE",
+        url: "/articles/" + thisId
+      })
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        location.reload();
+      }); 
+  });
+
